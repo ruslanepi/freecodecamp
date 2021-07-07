7bef247e5c8c6d4ccb9d67803ed00435b1194d0f
ruslanepi <ruslanep2@gmail.com>
2021-07-07 17:54:25 +0300

    update foundamentals project

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
 import './App.css'
 
+//SETUP VARS
+
+const firstBook = {
+title: 'firstBook',
+author: 'Lain Smith',
+img: "https://img3.labirint.ru/rc/54d5d5f5177bb0a0e013748414ab278f/220x340/books52/519519/cover.jpg?1563881242"
+}
+
+const secondBook = {
+title: 'secondBook',
+author: 'Ruslan Epishin',
+img: "https://img3.labirint.ru/rc/54d5d5f5177bb0a0e013748414ab278f/220x340/books52/519519/cover.jpg?1563881242"
+}
+
 function App() {
-  return <BookList />
+return <BookList />
 }
 
 const BookList = () => {
-  return (
-    <div>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </div>
-  )
+return (
+  
+  <div className="booklist">
+    <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
+    <p>description</p>
+    </Book>
+    <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
+    
+  </div>
+  
+)
 }
 
-const Book = () => {
-  return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
-      <hr></hr>
-    </article>
-  )
-}
-
-const Image = () => {
-  return <img alt='123' />
-}
-
-const Title = () => {
-  return <h1>title of my book</h1>
-}
-
-const Author = () => {
-  return <h4>Epishin Ruslan dmitriyevich</h4>
+const Book = ({img, title, author, children}) => {
+return (
+  <article className="book">
+    <img src={img} />
+    <h1>{title}</h1>
+    <h4>{author}</h4>
+    <p>{children}</p>
+    <br></br>
+  </article>
+)
 }
 
 export default App
