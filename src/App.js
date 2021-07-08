1ad3fa9f83bee6f3cce7daea07835f993f1867bd
ruslanepi <ruslanep2@gmail.com>
2021-07-08 17:41:44 +0300

    prepare to adwanced react

--- src/App.js
+++ src/App.js
@@ -1,47 +1,12 @@
-import './App.css'
+import "./App.css";
+import Setup from "./tutorial/1-useState/setup/1-error-example";
 
-//SETUP VARS
+const App = () => {
+  return (
+    <div className="container">
+      <Setup />
+    </div>
+  );
+};
 
-const firstBook = {
-title: 'firstBook',
-author: 'Lain Smith',
-img: "https://img3.labirint.ru/rc/54d5d5f5177bb0a0e013748414ab278f/220x340/books52/519519/cover.jpg?1563881242"
-}
-
-const secondBook = {
-title: 'secondBook',
-author: 'Ruslan Epishin',
-img: "https://img3.labirint.ru/rc/54d5d5f5177bb0a0e013748414ab278f/220x340/books52/519519/cover.jpg?1563881242"
-}
-
-function App() {
-return <BookList />
-}
-
-const BookList = () => {
-return (
-  
-  <div className="booklist">
-    <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
-    <p>description</p>
-    </Book>
-    <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
-    
-  </div>
-  
-)
-}
-
-const Book = ({img, title, author, children}) => {
-return (
-  <article className="book">
-    <img src={img} />
-    <h1>{title}</h1>
-    <h4>{author}</h4>
-    <p>{children}</p>
-    <br></br>
-  </article>
-)
-}
-
-export default App
+export default App;
---
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const ErrorExample = () => {
+
+  let title = 'random title'
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+    </React.Fragment>
+  )
+}
+
+export default ErrorExample
